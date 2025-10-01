19c58d4afe53ae2e6a58c000f4c7a8123f2e8f94
WataXScript <dalvo9216@gmail.com>
2025-10-01 16:40:44 +0800

    Update main.lua

--- Loader/main.lua
+++ Loader/main.lua
@@ -8,7 +8,7 @@ local urlSatuan = "https://raw.githubusercontent.com/WataXScript/VerifikasiScWat
 
 local successUrls = {
     "https://raw.githubusercontent.com/WataXScript/WataXMountAtin/main/Loader/WataX.lua",
-    "https://raw.githubusercontent.com/WataXScript/WataXMountDaun/main/Loader/mainmap926.lua"
+    "https://raw.githubusercontent.com/WataXScript/WataXMount700YntKts/refs/heads/main/Loader/mainmap437.lua"
 }
 
 
